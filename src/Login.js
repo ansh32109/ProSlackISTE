8bca9a20b3b3dec60af1a11bbd879dce18ee9903
Jubitha24 <140185836+Jubitha24@users.noreply.github.com>
2024-03-07 22:35:06 +0530

    Add files via upload

--- src/Login.js
+++ src/Login.js
@@ -1,32 +1,38 @@
+// Login.js
+
 import React from "react";
 import "./Login.css";
 import { Button } from "@mui/material";
-import{auth,provider} from "./firebase";
+import { auth, provider } from "./firebase";
 import { useStateValue } from "./StateProvider";
 import { actionTypes } from "./reducer";
 
 function Login() {
-    const[state,dispatch]=useStateValue();
-const signIn = () =>{
-    auth
-    .signInWithPopup(provider)
-    .then((result)=>{console.log(result);
-    dispatch({type:actionTypes.SET_USER,
-    user:result.user,
-    })})
-    .catch((error)=>{alert(error.message);});
-}
+    const [state, dispatch] = useStateValue();
 
-    return <div className="login">
-        <div className='Login__container'>
-            <img src="https://www.sketchappsources.com/resources/source-image/new-slack-logo-nicolas-ciotti.jpg" alt="SLACK" />
-         <h5>Sign in to SLACK</h5>
-         
-         <Button onClick={signIn}>Sign in with Google</Button>
-        
+    const signIn = () => {
+        auth.signInWithPopup(provider)
+            .then((result) => {
+                console.log(result);
+                dispatch({
+                    type: actionTypes.SET_USER,
+                    user: result.user,
+                });
+            })
+            .catch((error) => {
+                alert(error.message);
+            });
+    };
+
+    return (
+        <div className="login">
+            <div className='Login__container'>
+                <img src="https://www.sketchappsources.com/resources/source-image/new-slack-logo-nicolas-ciotti.jpg" alt="SLACK" />
+                <h5>Sign in to ProSlack</h5>
+                <Button onClick={signIn}>Sign in with Google</Button>
+            </div>
         </div>
-
-    </div>
+    );
 }
 
 export default Login;
